66e263820aba5877579bf94ecdb9f6dcbaa45d12
Lumy <blndogsun@hotmail.com>
2021-07-10 16:52:12 +0100

    buttons highlights on mouse hover

--- main/Wolf_and_Sheep/button.py
+++ main/Wolf_and_Sheep/button.py
@@ -12,6 +12,11 @@ class Button:
 
 
     def render(self, game_display):
+        x, y = pygame.mouse.get_pos()
+        if self.rect.collidepoint(x, y):
+            self.current_color = self.highlight_color
+        else:
+            self.current_color = self.normal_color
         pygame.draw.rect(game_display, self.current_color, self.rect, 2)
         text = self.font.get_rect(self.text)
         text_position = (self.rect.center[0] - text.width * 0.5, self.rect.center[1] - text.height * 0.5)
