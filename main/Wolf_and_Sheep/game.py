c77d30b2d1f53f9c91d34f3dbc54fb130698d266
Lumy <blndogsun@hotmail.com>
2021-07-10 17:45:36 +0100

    draw grid and players

--- main/Wolf_and_Sheep/game.py
+++ main/Wolf_and_Sheep/game.py
@@ -15,10 +15,35 @@ class Game:
 
         self.button = Button('Exit', self.font, pygame.Rect(10, 680, 100, 30), (255, 255, 0), (255, 255, 255), lambda: self.exit_to_menu())
 
+        self.grid_start = (384, 104)
+
+        self.grid = [[0, 1, 0, 1, 0, 1, 0, 1]]
+        for i in range(0, 6):
+            e = []
+            for j in range(0, 8):
+                e.append(0)
+            self.grid.append(e)
+        self.grid.append([0, 0, 0, 0, 2, 0, 0, 0])
+
+    def draw_grid(self, game_display):
+        draw_y = self.grid_start[1]
+        for y in range(0, 8):
+            draw_x = self.grid_start[0]
+            for x in range(0, 8):
+                pygame.draw.rect(game_display, (0, 255, 0), (draw_x, draw_y, 64, 64), 2)
+
+                if (self.grid[y][x] == 1):
+                    game_display.blit(self.wolf, (draw_x, draw_y))
+                elif (self.grid[y][x] == 2):
+                    game_display.blit(self.sheep, (draw_x, draw_y))
+                draw_x += 64
+            draw_y += 64
+
 
     def render(self, game_display):
         game_display.fill((0, 0, 20))
         self.button.render(game_display)
+        self.draw_grid(game_display)
 
     def on_mouse_up(self):
         x, y = pygame.mouse.get_pos()
