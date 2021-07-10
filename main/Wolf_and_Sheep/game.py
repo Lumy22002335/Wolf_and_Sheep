4b786f03a63e78bff90f3c48f1be66d77f959576
Lumy <blndogsun@hotmail.com>
2021-07-10 22:49:23 +0100

    Win condition, Sheep/Wold can now win

--- main/Wolf_and_Sheep/game.py
+++ main/Wolf_and_Sheep/game.py
@@ -33,7 +33,11 @@ class Game:
         # Set phase to 0 (0 = select Sheep or Wolf to move; 1 = select the tile to move to)
         self.phase = 0
 
-    def valid_movement(self, source, target):
+        self.end_state = 0
+
+        self.sheep_position = (4, 7)
+
+    def valid_movement(self, source, target, type):
         if source[0] == target[0] or source[1] == target[1]:
             return False
 
@@ -50,7 +54,7 @@ class Game:
         if self.grid[target[1]][target[0]] != 0:
             return False
 
-        if self.turn == 1:
+        if type == 1:
             if source[1] > target[1]:
                 return False
         return True
@@ -62,7 +66,7 @@ class Game:
             for x in range(0, 8):
 
                 if self.phase == 1:
-                    if self.valid_movement(self.selected_tile, (x, y)):
+                    if self.valid_movement(self.selected_tile, (x, y), self.turn):
                         pygame.draw.rect(game_display, (255, 255, 0), (draw_x, draw_y, 64, 64), 0)
 
                 pygame.draw.rect(game_display, (0, 255, 0), (draw_x, draw_y, 64, 64), 2)
@@ -80,9 +84,28 @@ class Game:
         self.button.render(game_display)
         self.draw_grid(game_display)
 
+        if self.end_state == 1:
+            self.font.render_to(game_display, (574, 50), 'WOLF WON!', (0, 255, 255))
+
+        if self.end_state == 2:
+            self.font.render_to(game_display, (570, 50), 'SHEEP WON!', (0, 255, 255))
+
     def update(self):
         x, y = pygame.mouse.get_pos()
         self.mouse_grid_pos = ((x - 384) // 64, (y - 104) // 64)
+
+        # If the Sheep reached the top of the grid, the Sheep won
+        if self.sheep_position[1] == 0:
+            self.end_state = 2
+
+        # If the Sheep has no movements the Wolf won
+        has_valid_movements = False
+        for i in range(0, 8):
+            for j in range(0, 8):
+                if self.valid_movement((self.sheep_position[0], self.sheep_position[1]), (i, j), 2):
+                    has_valid_movements = True
+        if not has_valid_movements:
+            self.end_state = 1
         
     def on_mouse_up(self):
         x, y = pygame.mouse.get_pos()
@@ -101,7 +124,7 @@ class Game:
         
         # If we're on the moving phase, check if the player clicked on a valid tile to move
         elif self.phase == 1:
-            if self.valid_movement(self.selected_tile, self.mouse_grid_pos):
+            if self.valid_movement(self.selected_tile, self.mouse_grid_pos, self.turn):
                 self.grid[self.mouse_grid_pos[1]][self.mouse_grid_pos[0]] = self.turn
                 self.grid[self.selected_tile[1]][self.selected_tile[0]] = 0
 
@@ -109,6 +132,7 @@ class Game:
                     self.turn = 2
                 else:
                     self.turn = 1
+                    self.sheep_position = (self.mouse_grid_pos[0], self.mouse_grid_pos[1])
                 self.phase = 0
 
     def exit_to_menu(self):
