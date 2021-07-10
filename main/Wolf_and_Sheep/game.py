fd47247e1fad0a208e6377360f6404090496395a
Lumy <blndogsun@hotmail.com>
2021-07-10 22:12:17 +0100

    limited player movement to valid tiles!

--- main/Wolf_and_Sheep/game.py
+++ main/Wolf_and_Sheep/game.py
@@ -33,11 +33,32 @@ class Game:
         # Set phase to 0 (0 = select Sheep or Wolf to move; 1 = select the tile to move to)
         self.phase = 0
 
+    def valid_movement(self, source, target):
+        if source[0] == target[0] or source[1] == target[1]:
+            return False
+
+        if target[0] < 0 or target[0] > 7:
+            return False
+        if target[1] < 0 or target[1] > 7:
+            return False
+
+        tile_dist = abs(target[0] - source[0]) + abs(target[1] - source[1])
+        if tile_dist != 2:
+            return False
+        if self.grid[target[1]][target[0]] != 0:
+            return False
+        return True
+
     def draw_grid(self, game_display):
         draw_y = self.grid_start[1]
         for y in range(0, 8):
             draw_x = self.grid_start[0]
             for x in range(0, 8):
+
+                if self.phase == 1:
+                    if self.valid_movement(self.selected_tile, (x, y)):
+                        pygame.draw.rect(game_display, (255, 255, 0), (draw_x, draw_y, 64, 64), 0)
+
                 pygame.draw.rect(game_display, (0, 255, 0), (draw_x, draw_y, 64, 64), 2)
 
                 if (self.grid[y][x] == 1):
@@ -74,14 +95,15 @@ class Game:
         
         # If we're on the moving phase, check if the player clicked on a valid tile to move
         elif self.phase == 1:
-            self.grid[self.mouse_grid_pos[1]][self.mouse_grid_pos[0]] = self.turn
-            self.grid[self.selected_tile[1]][self.selected_tile[0]] = 0
+            if self.valid_movement(self.selected_tile, self.mouse_grid_pos):
+                self.grid[self.mouse_grid_pos[1]][self.mouse_grid_pos[0]] = self.turn
+                self.grid[self.selected_tile[1]][self.selected_tile[0]] = 0
 
-            if self.turn == 1:
-                self.turn = 2
-            else:
-                self.turn = 1
-            self.phase = 0
+                if self.turn == 1:
+                    self.turn = 2
+                else:
+                    self.turn = 1
+                self.phase = 0
 
     def exit_to_menu(self):
         self.exit = True
